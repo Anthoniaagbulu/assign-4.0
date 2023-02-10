145221d4df226a13de64f995955e23c9376c6b05
anthoniaagbulu <thoniagbulu1st@gmail.com>
2023-02-10 02:43:49 -0800

    work

---
+++ index.js
@@ -0,0 +1,74 @@
+// Question1
+var arr=[["1", "2", "3"],["4", "5", "6"],"7", "8", "9"[["10", "11", "12",["13", "4", "112",]], "136", "14"],];
+
+// printing out numbers ("10", "8", "13", "136" and "4")
+var value1 = arr[6][0], [3],[7][0], [8], [1][0]; 
+
+console.log(value1)
+
+// question 2
+var arr =["javascript", "web", "CapacityBay"];
+var name = "web"
+var name1 = "CapacityBay"
+var name2 = "javascript"
+
+console.log("I am a ${arr[1]} developer at ${arr[2] and I love ${arr[0]}");
+
+// Question 3
+var arr =["G", "e", "n", "e", "r","a", "l", "i","z", "a", "t","i", "o","n"];
+
+var value = [10]
+
+console.log(value)
+
+//question 4
+var arr = ["pea", "banana",["orange", "apple", "guava", "lemon"],"Avocado", "mango"];
+
+//Section2
+Q . A 
+for (i =1; i <=100; i++) {
+
+    if ((i / 3==1) && (i / 5== 1)) {
+    console.log("FizzBuzz");
+    //(a)
+    } else if (i / 3==[1] && [1,2]) {
+    console.log("Fizz");
+    //(b)
+    } else if  (i / 5 ==[1] && [1,2]) {
+    console.log("Buzz");
+     }
+     else {
+    console.log(i);
+     }
+}
+
+//(c)
+if ((i / 3==[1,2])); {
+    console.log("fizz")
+}
+
+//divisible by 5
+if ((i / 5==[1,2])) {
+    console.log("Fizz")
+}
+else if ((i / 5==[4])){
+    console.log("Buzz")
+}
+else if ((i / 5==[7])){
+    console.log("Fizz")
+}
+else if ((i / 5==[8])){
+    console.log("Buzz")
+}
+else if ((i / 5==[11])){
+    console.log("Buzz")
+}
+else if ((i / 5==[13])){
+    console.log("Fizz")
+}
+else if ((i / 5==[14, 13])) {
+    console.log("Fizz")
+}
+    else if ((i / 5 && i / 3==)){
+        console.log("Fizzbuss")
+    }
